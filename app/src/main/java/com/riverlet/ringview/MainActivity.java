0c473bf71afbaad14501397df30b4e840c0815eb
riverlet.liu <riverlet.liu@qq.com>
2017-11-16 16:30:49 +0800

    修改状态栏颜色

--- app/src/main/java/com/riverlet/ringview/MainActivity.java
+++ app/src/main/java/com/riverlet/ringview/MainActivity.java
@@ -6,12 +6,13 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     AnnularChartView annularChartView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       annularChartView = findViewById(R.id.annularChartView);
+        annularChartView = findViewById(R.id.annularChartView);
         findViewById(R.id.text_1).setOnClickListener(this);
         findViewById(R.id.text_2).setOnClickListener(this);
         findViewById(R.id.text_3).setOnClickListener(this);
@@ -21,13 +22,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.text_1:
-                annularChartView.setData(new float[]{100f,100f,100f,100f,100f});
+                annularChartView.setData(new float[]{100f, 100f, 100f, 100f, 100f});
                 break;
             case R.id.text_2:
-                annularChartView.setData(new float[]{100f,200f,300f,400f,500f});
+                annularChartView.setData(new float[]{100f, 200f, 300f, 400f, 500f});
                 break;
             case R.id.text_3:
-                annularChartView.setData(new float[]{500f,100f,300f,100f,600f});
+                annularChartView.setData(new float[]{500f, 100f, 300f, 100f, 600f});
                 break;
         }
     }
